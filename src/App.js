29501bdfaf08c4a1912de78420aeeea6bf662f7c
Neetu981 <neetup281@gmail.com>
2023-05-13 17:33:14 +0530

    first_task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import ExpenceItem from './components/ExpenceItem';
+import Expenses from './components/Expenses/Expense';
 
-function App() {
-  const expense=[
+const App=()=> {
+  const expenses=[
     {id:'e1',title:'Car Insurance', amount:269.76, date:new Date(2023, 3, 28),LocationOfExpenditure: 'Panipat'},
     {id:'e2',title:'new tv', amount:57.76, date:new Date(2023, 6, 28),LocationOfExpenditure: 'Panipat'},
     {id:'e3',title:'Notebooks', amount:99.76, date:new Date(2023, 2, 28),LocationOfExpenditure: 'Panipat'},
@@ -26,10 +26,7 @@ function App() {
         >
           Learn React
         </a> */}
-        <ExpenceItem title={expense[0].title} amount={expense[0].amount} date={expense[0].date } LocationOfExpenditure={expense[0].LocationOfExpenditure}></ExpenceItem>
-        <ExpenceItem title={expense[1].title} amount={expense[1].amount} date={expense[1].date } LocationOfExpenditure={expense[1].LocationOfExpenditure}></ExpenceItem>
-        <ExpenceItem title={expense[2].title} amount={expense[2].amount} date={expense[2].date } LocationOfExpenditure={expense[2].LocationOfExpenditure}></ExpenceItem>
-        <ExpenceItem title={expense[3].title} amount={expense[3].amount} date={expense[3].date } LocationOfExpenditure={expense[3].LocationOfExpenditure}></ExpenceItem>
+        <Expenses expense={expenses}></Expenses>
   
       </header>
     </div>
